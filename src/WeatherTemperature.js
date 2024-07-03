607104fbcc844e1e96e8e7dbcfd6a5cb2ae205f2
Nadika <nadika.name@gmail.com>
2024-07-03 12:11:37 +0100

    Update WeatherTemperature.js

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 export default function WeatherTemperature(props){
-  const[unit, setUnit] = useState('metrics');
+  const[unit, setUnit] = useState('celsius');
   function showFahrenheit(event){
     event.preventDefault();
     setUnit("fahrenheit");
